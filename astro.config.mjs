e795e9fdc6fe43341faf981cdc442c51d880d590
Rei <rei@example.com>
2025-04-13 20:55:18 +0900

    fix: update content config and fix rss/index.xml.ts module error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,36 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import rehypeSlug from "rehype-slug";
-import externalLinks from "remark-external-links"; // ✅ 追加
+import externalLinks from "remark-external-links";
+import content from '@astrojs/content';
 
 export default defineConfig({
   base: '/',
   site: 'https://toriscript.com',
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    content({
+      collections: {
+        articles: {
+          type: 'content',
+          schema: ({ z }) =>
+            z.object({
+              title: z.string(),
+              pubDate: z.string(),
+              category: z.string(),
+              categoryLabel: z.string().optional(),
+              description: z.string().optional(),
+              intro: z.string().optional(),
+              introImage: z.string().optional()
+            }),
+        },
+      },
+    }),
+  ],
   markdown: {
     rehypePlugins: [rehypeSlug],
     remarkPlugins: [
-      [externalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }] // ✅ 外部リンクに対応
+      [externalLinks, { target: '_blank', rel: ['noopener', 'noreferrer'] }]
     ],
-  },
-  content: {
-    collections: {
-      articles: {
-        type: 'content',
-        schema: ({ z }) =>
-          z.object({
-            title: z.string(),
-            pubDate: z.string(),
-            category: z.string(),
-            categoryLabel: z.string().optional(),
-            description: z.string().optional(),
-            intro: z.string().optional(),
-            introImage: z.string().optional()
-          }),
-      },
-    },
   },
 });
